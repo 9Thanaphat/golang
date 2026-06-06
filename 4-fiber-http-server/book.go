ee744ddb020ad9ae39101b67eac499f8a344e5bd
9Thanaphat <tangcham.thanaphat@gmail.com>
2026-06-06 21:12:23 +0900

    feat: add HTML template rendering and file upload

---
+++ 4-fiber-http-server/book.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func getBooks(c fiber.Ctx) error {
+	return c.JSON(books)
+}
+
+func getBook(c fiber.Ctx) error {
+	bookId, err := strconv.Atoi(c.Params("id"))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	for _, book := range books {
+		if book.ID == bookId {
+			return c.JSON(book)
+		}
+	}
+	return c.Status(fiber.StatusNotFound).SendString("Book not found")
+}
+
+func createBook(c fiber.Ctx) error {
+	book := new(Book)
+	if err := c.Bind().Body(book); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+	books = append(books, *book)
+	return c.JSON(book)
+}
+
+func updateBook(c fiber.Ctx) error {
+	bookId, err := strconv.Atoi(c.Params("id"))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	bookUpdate := new(Book)
+	if err := c.Bind().Body(bookUpdate); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	for i, book := range books {
+		if book.ID == bookId {
+			books[i].Title = bookUpdate.Title
+			books[i].Author = bookUpdate.Author
+			return c.JSON(books[i])
+		}
+	}
+	return c.Status(fiber.StatusNotFound).SendString("Book not found")
+}
+
+func deleteBook(c fiber.Ctx) error {
+	bookId, err := strconv.Atoi(c.Params("id"))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	for i, book := range books {
+		if book.ID == bookId {
+			// สมมติว่าเจอหนังสือที่ตรงกับ bookId ในรอบที่ i พอดี
+			// เราจะทำการหั่น (Slice) อาเรย์เดิมออกเป็น 2 ท่อน:
+
+			// ท่อนที่ 1: books[:i]
+			// คือการดึงข้อมูลตั้งแต่ตัวแรกสุด มาจนถึง "ก่อน" ตำแหน่ง i
+
+			// ท่อนที่ 2: books[i+1:]
+			// คือการดึงข้อมูลตั้งแต่ "หลัง" ตำแหน่ง i (ก็คือ i+1) ไปจนถึงตัวสุดท้าย
+
+			// เครื่องหมาย ... (Spread/Unpack operator)
+			// ทำหน้าที่แตกข้อมูลในท่อนที่ 2 ออกมาทีละชิ้น เพื่อให้ฟังก์ชัน append() รับเข้าไปได้
+
+			// สุดท้ายจับท่อน 1 ต่อด้วยท่อน 2 แล้วเอาไปทับตัวแปร books ตัวเดิม
+			books = append(books[:i], books[i+1:]...)
+			return c.SendStatus(fiber.StatusNoContent)
+		}
+	}
+	return c.Status(fiber.StatusNotFound).SendString("Book not found")
+}
--- 4-fiber-http-server/main.go
+++ 4-fiber-http-server/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/template/html/v2"
 )
 
 type Book struct {
@@ -16,7 +16,16 @@ type Book struct {
 var books []Book
 
 func main() {
-	app := fiber.New()
+	// init engine
+	engine := html.New("./views", ".html")
+
+	// pass engine to fiber
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	// Setup route
+	app.Get("/", renderTemplate)
 
 	app.Get("/hello", func(c fiber.Ctx) error {
 		return c.SendString("Hello World!")
@@ -33,86 +42,28 @@ func main() {
 	app.Put("/books/:id", updateBook)
 	app.Delete("/books/:id", deleteBook)
 
+	app.Post("/upload", uploadFile)
+
 	app.Listen(":8080")
 	fmt.Println("Server is running on http://localhost:8080")
 
 }
 
-func getBooks(c fiber.Ctx) error {
-	return c.JSON(books)
+func renderTemplate(c fiber.Ctx) error {
+	// Render the template with variable data
+	return c.Render("template", fiber.Map{
+		"Name": "9Thanaphat",
+	})
 }
 
-func getBook(c fiber.Ctx) error {
-	bookId, err := strconv.Atoi(c.Params("id"))
-
+func uploadFile(c fiber.Ctx) error {
+	file, err := c.FormFile("image")
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
 	}
-
-	for _, book := range books {
-		if book.ID == bookId {
-			return c.JSON(book)
-		}
-	}
-	return c.Status(fiber.StatusNotFound).SendString("Book not found")
-}
-
-func createBook(c fiber.Ctx) error {
-	book := new(Book)
-	if err := c.Bind().Body(book); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-	books = append(books, *book)
-	return c.JSON(book)
-}
-
-func updateBook(c fiber.Ctx) error {
-	bookId, err := strconv.Atoi(c.Params("id"))
-
+	err = c.SaveFile(file, "./uploads/"+file.Filename)
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 	}
-
-	bookUpdate := new(Book)
-	if err := c.Bind().Body(bookUpdate); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-
-	for i, book := range books {
-		if book.ID == bookId {
-			books[i].Title = bookUpdate.Title
-			books[i].Author = bookUpdate.Author
-			return c.JSON(books[i])
-		}
-	}
-	return c.Status(fiber.StatusNotFound).SendString("Book not found")
-}
-
-func deleteBook(c fiber.Ctx) error {
-	bookId, err := strconv.Atoi(c.Params("id"))
-
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-
-	for i, book := range books {
-		if book.ID == bookId {
-			// สมมติว่าเจอหนังสือที่ตรงกับ bookId ในรอบที่ i พอดี
-			// เราจะทำการหั่น (Slice) อาเรย์เดิมออกเป็น 2 ท่อน:
-
-			// ท่อนที่ 1: books[:i]
-			// คือการดึงข้อมูลตั้งแต่ตัวแรกสุด มาจนถึง "ก่อน" ตำแหน่ง i
-
-			// ท่อนที่ 2: books[i+1:]
-			// คือการดึงข้อมูลตั้งแต่ "หลัง" ตำแหน่ง i (ก็คือ i+1) ไปจนถึงตัวสุดท้าย
-
-			// เครื่องหมาย ... (Spread/Unpack operator)
-			// ทำหน้าที่แตกข้อมูลในท่อนที่ 2 ออกมาทีละชิ้น เพื่อให้ฟังก์ชัน append() รับเข้าไปได้
-
-			// สุดท้ายจับท่อน 1 ต่อด้วยท่อน 2 แล้วเอาไปทับตัวแปร books ตัวเดิม
-			books = append(books[:i], books[i+1:]...)
-			return c.SendStatus(fiber.StatusNoContent)
-		}
-	}
-	return c.Status(fiber.StatusNotFound).SendString("Book not found")
+	return c.Status(fiber.StatusCreated).SendString("File uploaded successfully")
 }
